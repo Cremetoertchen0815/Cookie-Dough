2bcda09f2d52ff163acaf4f12b31500dd7f017da
Cremetoertchen0815 <Cremetoertchen0815@users.noreply.github.com>
2021-05-16 12:48:51 +0200

    Updated version string for compatibility

--- Cookie Dough/Program.vb
+++ Cookie Dough/Program.vb
@@ -11,7 +11,7 @@ Namespace Cookie_Dough
     ' </summary>
     Public Module Program
         'Old Beacon-tweening system for backwards compatibility for BV
-        Friend Property VersionString As String = "Cookie Dough V0.23"
+        Friend Property VersionString As String = "Cookie Dough V0.24"
         Friend Property Automator As TweenManager
         Friend Property ReferencePixel As Texture2D
         Friend Property Dev As GraphicsDevice
