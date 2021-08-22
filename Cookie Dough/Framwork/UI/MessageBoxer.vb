ff61a04f45b15ca593a9c6f243da3df22fd43bc1
Creme <Mircosoft_heck@web.de>
2021-08-22 12:57:35 +0200

    Implemented handling a messagebox queue

--- Cookie Dough/Framwork/UI/MessageBoxer.vb
+++ Cookie Dough/Framwork/UI/MessageBoxer.vb
@@ -1,4 +1,5 @@
-﻿Imports Microsoft.Xna.Framework
+﻿Imports System.Collections.Generic
+Imports Microsoft.Xna.Framework
 Imports Microsoft.Xna.Framework.Graphics
 Imports Microsoft.Xna.Framework.Input
 
@@ -21,10 +22,10 @@ Namespace Framework.UI
 
         'Layout & data
         Private MsgBoxArea As New Rectangle(710, 440, 500, 170)
-        Public Style As MessageBoxStyle = MessageBoxStyle.Blali
         Private CurrentMessage As Message = New Message With {.IsInputbox = False, .Message = "OwO, what's this? lololololololololololololololololololololololololololololololololololol", .Buttons = {"A", "Lang", "Länger"}, .FinalAction = Nothing}
         Private ShowMessageBox As Boolean = False
         Private WasEnabled As Boolean
+        Private MessageStack As New List(Of Message)
 
         Sub New()
             batcher = New Batcher(Core.GraphicsDevice)
@@ -36,53 +37,51 @@ Namespace Framework.UI
             If Not ShowMessageBox Then Return
 
             batcher.Begin(Material, ScaleMatrix)
-            Select Case Style
-                Case MessageBoxStyle.Blali
 
-                    If CurrentMessage.IsInputbox Then
-                    Else
-                        'Draw base
-                        batcher.DrawRect(MsgBoxArea, New Color(90, 90, 90))
-                        batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Top), New Vector2(MsgBoxArea.Left, MsgBoxArea.Bottom), New Color(20, 20, 20), 4)
-                        batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Top), New Vector2(MsgBoxArea.Right, MsgBoxArea.Top), New Color(20, 20, 20), 3)
-                        batcher.DrawLine(New Vector2(MsgBoxArea.Right, MsgBoxArea.Top), New Vector2(MsgBoxArea.Right, MsgBoxArea.Bottom), New Color(180, 180, 180), 4)
-                        batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Bottom), New Vector2(MsgBoxArea.Right, MsgBoxArea.Bottom), New Color(180, 180, 180), 3)
+            If CurrentMessage.IsInputbox Then
+            Else
+                'Draw base
+                batcher.DrawRect(MsgBoxArea, New Color(90, 90, 90))
+                batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Top), New Vector2(MsgBoxArea.Left, MsgBoxArea.Bottom), New Color(20, 20, 20), 4)
+                batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Top), New Vector2(MsgBoxArea.Right, MsgBoxArea.Top), New Color(20, 20, 20), 3)
+                batcher.DrawLine(New Vector2(MsgBoxArea.Right, MsgBoxArea.Top), New Vector2(MsgBoxArea.Right, MsgBoxArea.Bottom), New Color(180, 180, 180), 4)
+                batcher.DrawLine(New Vector2(MsgBoxArea.Left, MsgBoxArea.Bottom), New Vector2(MsgBoxArea.Right, MsgBoxArea.Bottom), New Color(180, 180, 180), 3)
 
-                        'Draw text
-                        batcher.DrawString(DispFont, StaticFunctions.WrapTextDifferently(CurrentMessage.Message, 49, False), MsgBoxArea.Location.ToVector2 + New Vector2(30, 10), Color.White)
+                'Draw text
+                batcher.DrawString(DispFont, StaticFunctions.WrapTextDifferently(CurrentMessage.Message, 49, False), MsgBoxArea.Location.ToVector2 + New Vector2(30, 10), Color.White)
 
-                        'Get total button length
-                        Dim tot_len As Single = 0F
-                        Dim padding As Single = 25.0F
-                        Dim margin As Single = 30.0F
-                        For i As Integer = 0 To CurrentMessage.Buttons.Length - 1
-                            tot_len += DispFont.MeasureString(CurrentMessage.Buttons(i)).X + padding 'width + padding
-                        Next
-                        tot_len += (CurrentMessage.Buttons.Length - 1) * margin 'Add margin
+                'Get total button length
+                Dim tot_len As Single = 0F
+                Dim padding As Single = 25.0F
+                Dim margin As Single = 30.0F
+                For i As Integer = 0 To CurrentMessage.Buttons.Length - 1
+                    tot_len += DispFont.MeasureString(CurrentMessage.Buttons(i)).X + padding 'width + padding
+                Next
+                tot_len += (CurrentMessage.Buttons.Length - 1) * margin 'Add margin
 
-                        'Draw buttons
-                        Dim x_offset As Single = 0F
-                        For i As Integer = 0 To CurrentMessage.Buttons.Length - 1
-                            Dim txt_width As Single = DispFont.MeasureString(CurrentMessage.Buttons(i)).X + padding
-                            Dim rect As New Rectangle(MsgBoxArea.Center.X - tot_len / 2 + x_offset, MsgBoxArea.Y + MsgBoxArea.Height * 0.7, txt_width, 30)
-                            batcher.Draw(ButtonBase, rect, Color.White)
-                            batcher.DrawString(DispFont, CurrentMessage.Buttons(i), rect.Location.ToVector2 + New Vector2(padding / 2, 5), Color.White)
+                'Draw buttons
+                Dim x_offset As Single = 0F
+                For i As Integer = 0 To CurrentMessage.Buttons.Length - 1
+                    Dim txt_width As Single = DispFont.MeasureString(CurrentMessage.Buttons(i)).X + padding
+                    Dim rect As New Rectangle(MsgBoxArea.Center.X - tot_len / 2 + x_offset, MsgBoxArea.Y + MsgBoxArea.Height * 0.7, txt_width, 30)
+                    batcher.Draw(ButtonBase, rect, Color.White)
+                    batcher.DrawString(DispFont, CurrentMessage.Buttons(i), rect.Location.ToVector2 + New Vector2(padding / 2, 5), Color.White)
 
-                            'Update offset
-                            x_offset += txt_width + margin
-                        Next
-                    End If
-
-            End Select
+                    'Update offset
+                    x_offset += txt_width + margin
+                Next
+            End If
             batcher.End()
         End Sub
 
         Dim lastmstate As MouseState
         Public Overrides Sub Update()
-            If Mouse.GetState.RightButton And Not ShowMessageBox Then
+            If MessageStack.Count > 0 And Not ShowMessageBox Then
                 WasEnabled = Core.Scene.Enabled
                 Core.Scene.Enabled = False
                 ShowMessageBox = True
+                CurrentMessage = MessageStack(0)
+                MessageStack.RemoveAt(0)
             End If
 
             If Not ShowMessageBox Then Return
@@ -117,14 +116,14 @@ Namespace Framework.UI
         End Sub
 
         Private Sub CloseMsgBox()
-            Core.Scene.Enabled = WasEnabled
-            ShowMessageBox = False
+            If MessageStack.Count > 0 Then
+                CurrentMessage = MessageStack(0)
+                MessageStack.RemoveAt(0)
+            Else
+                Core.Scene.Enabled = WasEnabled
+                ShowMessageBox = False
+            End If
         End Sub
-
-        Public Enum MessageBoxStyle
-            Blali
-            CookieDough
-        End Enum
 
         Private Structure Message
             Public Message As String
