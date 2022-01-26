dca89d64fd9eaef26819513e6d51436fa3ceafe6
Creme <Mircosoft_heck@web.de>
2022-01-26 16:43:39 +0100

    BV: Changed color back to default

--- Cookie Dough/Game/Betreten Verboten/GameRoom.vb
+++ Cookie Dough/Game/Betreten Verboten/GameRoom.vb
@@ -180,11 +180,8 @@ Namespace Game.BetretenVerboten
                 playcolor = hudcolors
                 Farben = {"Kamerad A1", "Kamerad B1", "Kamerad A2", "Kamerad B2", "Kamerad A3", "Kamerad B3", "Kamerad A4", "Kamerad B4"}
             Else
-                'hudcolors = {Color.Magenta, Color.Lime, Color.Cyan, Color.Orange, New Color(255, 32, 32), New Color(48, 48, 255), Color.Teal, New Color(85, 120, 20)}
-                'playcolor = {Color.Magenta, Color.Lime, Color.Cyan, Color.Yellow, Color.Maroon * 1.5F, New Color(0, 0, 200), New Color(0, 80, 80), New Color(85, 120, 20)}
-                'Farben = {"Magenta", "Lime", "Cyan", "Yellow", "Red", "Blue", "Teal", "Olive"}
-                hudcolors = {Color.White, Color.Black, New Color(40, 0, 75), New Color(0, 255, 100), New Color(255, 32, 32), New Color(48, 48, 255), Color.Teal, New Color(85, 120, 20)}
-                playcolor = {Color.White, Color.Black, New Color(40, 0, 75), New Color(0, 255, 100), Color.Maroon * 1.5F, New Color(0, 0, 200), New Color(0, 80, 80), New Color(85, 120, 20)}
+                hudcolors = {Color.Magenta, Color.Lime, Color.Cyan, Color.Orange, New Color(255, 32, 32), New Color(48, 48, 255), Color.Teal, New Color(85, 120, 20)}
+                playcolor = {Color.Magenta, Color.Lime, Color.Cyan, Color.Yellow, Color.Maroon * 1.5F, New Color(0, 0, 200), New Color(0, 80, 80), New Color(85, 120, 20)}
                 Farben = {"Magenta", "Lime", "Cyan", "Yellow", "Red", "Blue", "Teal", "Olive"}
             End If
 
